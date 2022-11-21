d3d6f622fdcb4c1aedaaccb8b0a3f87b1d2844e6
parreh <118777507+parreh@users.noreply.github.com>
2022-11-21 17:43:06 -0500

    Update Table_EvenNumbers.lua

--- Table_EvenNumbers.lua
+++ Table_EvenNumbers.lua
@@ -2,9 +2,9 @@
 
 local a = {1,2,3,4,5,6,7,8,9,10,11,12}
 local b = 0
-for i,v in pairs(a) do 
-  if (v % 2 == 0) then
-     b = b + v
+for i,v in pairs(a) do --get table 
+  if (v % 2 == 0) then  --determine if even
+     b = b + v       --adds all v from table)
      end
 end
-print(b)
+print(b) --Output: 42
